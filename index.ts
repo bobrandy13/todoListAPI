34b2ddd6040a0319d6ea16aef28880718366f417
Kevin Huang <khuang891@gmail.com>
2023-05-15 16:14:08 +1000

    changed it to add prisma and other stuff like apis

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
-import express from "express"
-const app = express() 
+const express = require("express")
+import {PrismaClient}
 
-// ask the user for type of command. 
-const outputString = 
-` Welcome to my todo List backend application. 
-  Enter a command to get started. 
-  1. Create a new todo
-  2. View Todos.
-  3. Update Todos. 
-  4. Delete Todos. 
-`
+const app = express()
 
-console.log(outputString)
+app.get("/read", () => {
+    res.status(200)
+})
 
 
+app.listen("3000", () => {
+    console.log("listening on port 3000")
+})
