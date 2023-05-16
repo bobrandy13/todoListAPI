36ea2cc26195a2c2609b761fe79e4180d39a8852
Kevin Huang <khuang891@gmail.com>
2023-05-16 16:46:15 +1000

    second commit

--- index.ts
+++ index.ts
@@ -1,13 +1,41 @@
-const express = require("express")
-import {PrismaClient}
+import { PrismaClient } from "@prisma/client";
+const express = require("express");
+import { Request, Response } from "express";
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient();
+console.log("hi");
 
-app.get("/read", () => {
-    res.status(200)
-})
+app.use(express.json());
 
+
+
+// main()
+
+app.get("/read", (req: Request, res: Response) => {
+  (async function () {
+    const readData = await prisma.todos.findMany();
+    console.log(readData);
+    res.json(readData)
+  })()
+});
+
+app.post("/posts", (req: Request, res: Response) => {
+  const { body } = req;
+  console.log(body);
+  res.send("post required to the  homepage");
+  (async function () {
+    const postData = await prisma.todos.create({
+        data: {
+            title: body.title,
+            body: body.body,
+        }
+    });
+  })();
+});
 
 app.listen("3000", () => {
-    console.log("listening on port 3000")
-})
+  console.log("listening on port 3000");
+});
+
+
